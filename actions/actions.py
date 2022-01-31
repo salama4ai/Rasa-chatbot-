1d4d6b74fc65adb69647fa04ca7876726ecf7376
salama4ai <salama4ai@gmail.com>
2022-01-31 22:53:58 +0200

    doing most of the work in nlu, domain, and rule files

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,55 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+ from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+ from rasa_sdk import Action, Tracker
+ from rasa_sdk.events import SlotSet
+ from rasa_sdk.executor import CollectingDispatcher
+ from rasa_sdk.types import DomainDict
 #
 #
-# class ActionHelloWorld(Action):
+
+countries_cap = {
+    "usa":"Washington, D.C.",
+    "greece": "Athens",
+    "sweden": "Stockholm",
+    "australia": "Canberra",
+    "finland": "Helsinki",
+    "japan": "Tokyo",
+    "russia": "Moscow",
+    "india": "New Delhi",   
+    }
+
+countries_pop = {
+    "usa": 330,
+    "greece": 10.7,
+    "sweden": 10.35,
+    "australia": 25.7,
+    "finland": 5.5,
+    "japan": 126,
+    "russia": 144,
+    "india": 1380,   
+    }
+
+ class ValidateUserQuestion(FormValidationAction):
 #
-#     def name(self) -> Text:
-#         return "action_hello_world"
+     def name(self) -> Text:
+         return "validate_user_question"
 #
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+     def validate_pop_cap(
+             self,
+             slot_value: Any,
+             dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: DomainDict
+             ) -> Dict[Text, Any]:
+
+         if slot_value.lower() not in countries_pop:
+             dispatcher.utter_message(text=)
+             return {"country": None}
+             
+         dispatcher.utter_message(text="Hello World!")
+         state = next(tracker.get_latest_entity_values(country), None)
+
+         return [SlotSet("country", state)]
