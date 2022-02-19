06de1036d279c889736fa9ef32c0a33c94d701aa
salama4ai <salama4ai@gmail.com>
2022-02-19 17:02:10 +0200

    solving the problem of getting the data from the endpoint and reducing the size of the directory from 800M to 100M by deleting the '.rasa' file then retrain

--- actions/actions.py
+++ actions/actions.py
@@ -13,33 +13,16 @@ from rasa_sdk.events import SlotSet, EventType
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict 
 import requests
+import json
+#from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
 #import jwt
 
-
-countries_cap = {
-    "Aa": "ccaappiittaall",
-    "USA":"Washington, D.C.",
-    "Greece": "Athens",
-    "Sweden": "Stockholm",
-    "Australia": "Canberra",
-    "Finland": "Helsinki",
-    "Japan": "Tokyo",
-    "Russia": "Moscow",
-    "India": "New Delhi"}
-
-countries_pop = {
-    "Aa": "1198798798790",
-    "USA": "330",
-    "Greece": "10.7",
-    "Sweden": "10.35",
-    "Australia": "25.7",
-    "Finland": "5.5",
-    "Japan": "126",
-    "Russia": "144",
-    "India": "1380"}
-
-base_url = "https://qcooc59re3.execute-api.us-east-1.amazonaws.com/dev/get"
+endpoint = "https://qcooc59re3.execute-api.us-east-1.amazonaws.com/dev/get"
 timeout = 55
+
+#auth = BotoAWSRequestsAuth(aws_host="qcooc59re3.execute-api.us-east-1.amazonaws.com",
+#                           aws_region="us-east-1", aws_service="execute-api")
+
 
 class ValidateUserQuestion(FormValidationAction):
     
@@ -51,115 +34,96 @@ class ValidateUserQuestion(FormValidationAction):
                          domain: DomainDict, slot_value: Any) -> List[Dict[Text, Any]]:
         """ validate the 'country' value"""
         
-        print("validate_country")
+        print("validate_country")  # to notify me when this function is called
         # check if the country slot value is not null
         if not slot_value:
-            print("slot_value_country is None")            
             dispatcher.utter_message(response = "utter_ask_country")
             return {"country": None}              
             
         # check if the country slot value is in our database  in case the user give country
         country = slot_value.title()         
         try:
-            get_countries = requests.get(f"{base_url}Countries", timeout=timeout)                                             
+            res_country = requests.get(f"{endpoint}Countries", timeout=timeout)
             # check if the status_code is ok, less than 400 
-            if not get_countries.ok:
-                print("connection is not ok")                            
+            if not res_country.ok:
                 raise Exception
-            elif country not in get_countries.json()["body"]:
-                print("country not found")                            
+            elif country not in res_country.json()["body"]:
                 dispatcher.utter_message(response = "utter_not_found", country=country)
                 return {"country": None}  
             else:
-                print("country slot is ok")                            
-                return {"country", country}                      
+                return {"country", country}
         except:
             dispatcher.utter_message(response = "utter_server_failure")
-            return {"country": None}            
+            return {"country": None}
 
 
     def validate_pop_cap(self, dispatcher: CollectingDispatcher, tracker: Tracker, 
                          domain: DomainDict, slot_value: Any) -> List[Dict[Text, Any]]:
         """ validate the 'pop_cap' value"""
         
-        print("validate_pop_cap")            
-        try:            
-            pop_cap = slot_value.title()    
+        print("validate_pop_cap")  # to notify me when this function is called
+        try:
+            pop_cap = slot_value.title()
             if (pop_cap in ["Capital", "Population"]):
-                print("pop_cap is correct")                            
-                return {"pop_cap": pop_cap} 
+                return {"pop_cap": pop_cap}
             else:
                 raise Exception()
-        except:  
-            print("pop_cap exception")                                                 
+        except:
             dispatcher.utter_message(response = "utter_ask_pop_cap",
                                      buttons=[{"title": "Capital", 
-                                              "payload": '/inform{{"pop_cap":"Capital"}}'},     
+                                              "payload": '/inform{"pop_cap":"Capital"}'},     
                                               {"title": "Population",
-                                              "payload": '/inform{{"pop_cap":"Population"}}'}])
-            return {"pop_cap": None}            
-
-
-
-class ActionAnswerTemporarly(Action):
-    """this function is temporal function untill i find the correct link to the get_capital and get_population"""
-
-    
-    def name(self) -> Text:
-        return "temporarly_action_answer"
-    
-
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, 
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        # i couldn't make the validation functions work by any means, so i call them explicitly
-        # till i fix them issue
-        ValidateUserQuestion.validate_country(self, dispatcher, tracker, domain, tracker.get_slot("country"))        
-        ValidateUserQuestion.validate_pop_cap(self, dispatcher, tracker, domain, tracker.get_slot("pop_cap"))
-
-        country = tracker.get_slot("country").title()
-        pop_cap = tracker.get_slot("pop_cap").title()        
-        print("temporarly_action_answer", country, pop_cap)
-
-        # the action when the country is selected and pop_cap=Capital
-        if pop_cap=="Capital":  
-            print(f"country is {country}, pop_cap is {pop_cap}")                                             
-            dispatcher.utter_message(response = "utter_answer_cap", 
-                                     country = country, 
-                                     cap = countries_cap[country])
-                                        
-        # the action when the country is selected and pop_cap=Population
-        elif pop_cap=="Population":
-            print(f"country is {country}, pop_cap is {pop_cap}")                                             
-            dispatcher.utter_message(response = "utter_answer_pop", 
-                                     country = country, 
-                                     pop = countries_pop[country])                      
-      
-        return[]
+                                              "payload": '/inform{"pop_cap":"Population"}'}])
+            return {"pop_cap": None}          
     
     
 class ActionAnswer(Action):
+    
 
     def name(self) -> Text:
         return "action_answer"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, 
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-         
-        country = tracker.get_slot("country")
-        pop_cap = tracker.get_slot("pop_cap")                       
 
-        # the action when the country is selected and pop_cap=capital
-        print("action_answer")
-        if pop_cap=="Capital": 
-            payload_cap = {}                         
-            try:                
-                get_capital = requests.get(f"{base_url}Capital", 
-                                           timeout=timeout, params=payload_cap)
+
+
+
+        print("program now in ActionAnswer.run()")
+        # i couldn't make the validation functions work by any means, so i call
+        # them explicitly till i could fix them issue        
+        ###############################################################
+        # to be deleted after i could fix the validation method issue        
+        ValidateUserQuestion.validate_pop_cap(self, dispatcher, tracker, domain, tracker.get_slot("pop_cap"))
+        country = ValidateUserQuestion.validate_country(self, dispatcher, tracker, domain, tracker.get_slot("country"))
+        if not country:
+            return
+        ################################################################
+
+
+
+
+
+        country = tracker.get_slot("country")
+        pop_cap = tracker.get_slot("pop_cap")              
+        payload = {"country": country}
+        #headers = {'Content-Type': 'application/json'}          
+        
+        # the action when the country is selected and pop_cap=capital        
+        if pop_cap=="Capital":
+            try:
+                res_cap = requests.post(f"{endpoint}Capital", 
+                                        json=payload, 
+                                        timeout=timeout,
+                                        #headers=headers, 
+                                        #auth=auth
+                                        )
                 # check if the status_code is ok, i.e less than 400 
-                if not get_capital.ok:
-                    raise Exception
-                cap = get_capital.json()["body"]["capital"]
+                # or i can not checking this it will raise an error any way
+                if not res_cap.ok:
+                    raise Exception             # res_cap.raise_for_status()
+                # getting the response of capital
+                cap = res_cap.json()["body"]["capital"]
                 dispatcher.utter_message(response = "utter_answer_cap", 
                                          country = country, cap = cap)
             except:
@@ -168,16 +132,20 @@ class ActionAnswer(Action):
                                         
         # the action when the country is selected and pop_cap=population
         elif pop_cap=="Population":
-            payload_pop = {}
             try:
-                get_population = requests.get(f"{base_url}Population", 
-                                              timeout=timeout, params=payload_pop)                                              
+                res_pop = requests.post(f"{endpoint}Population", 
+                                        json=payload, 
+                                        timeout=timeout,
+                                        #headers=headers, 
+                                        #auth=auth
+                                        )
                 # check if the status_code is ok, i.e less than 400 
-                if not get_population.ok:
+                if not res_pop.ok:
                     raise Exception
-                pop = get_population.json()["body"]["population"]
+                # getting the response of population
+                pop = res_pop.json()["body"]["population"]
                 dispatcher.utter_message(response = "utter_answer_pop", 
                                          country = country, pop = pop)
             except:
-                dispatcher.utter_message(response = "utter_server_failure")                       
-        return    
+                dispatcher.utter_message(response = "utter_server_failure")         
+        return
